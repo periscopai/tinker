453526604bc2883e238c6f6db8f01761e4ad194a
Laurent Brack <laurent.brack@gmail.com>
2020-10-04 20:53:25 -0700

    Starting to implement the RTSP pipeline ... not much at this point

--- doc/gstreamer_c_pipeline.md
+++ doc/gstreamer_c_pipeline.md
@@ -74,6 +74,35 @@ if you run this example you should get something like this
 
 ![gstreamer intro](images/gst-intro.png)
 
+# GSStreame Foundations
+
+The following is taken from [GStreamer Foundations](https://gstreamer.freedesktop.org/documentation/application-development/introduction/basics.html?gi-language=c#)
+
+But in an nutshell, there are 2 fundamental construct in GStreamer; **Elements** and **Pads**. **Elements** are responsible for processing 
+data and **Pads** provide the data connections between **Elements**. **Pads** can either be *sink* or *source*. Those connections are negotiated through *Capacities* (GstCaps)
+
+There are three types of **Elements**:
+
+  - **Sources** - this is where the data originate from (e.g. file)
+  - **Filters** - data processing elements such as demuxer, decoders, etc
+  - **Sink** - this is where the transformed data ends up (display, streaming, etc)
+
+![gstreamer pipeline](images/gstreamer-pipeline.png)
+
+**Bins** are containers of **Elements** and are **Elements** themselves, being subclasses of **Elements**. The purpose of **Bins** is 
+to abstract behavior that can not be implemented as a single **Element**. For example, an encoder may consist of multiple **Elements**
+(pre-processing, encoding, quantization, rate control) but should be percieved as a single **Element** in the pipeline. 
+
+**Pipelines** are top level **Bins**.
+
+Finally, GStreamer offers 4 communication mechanism: *Buffers*, *Events*, *Messages* and *Queries*. You can read the details on the 
+link aforementioned (as the purpose here is not to repeat what is already documented), but those should be pretty straight forward
+for the picture blow
+
+![gstreamer communication](images/gstreamer-communication.png)
+
+- [More on Element](https://gstreamer.freedesktop.org/documentation/application-development/basics/elements.html)
+
 # Examples
 
 To be started.
---
+++ gstreamer/rtsp/gst-rtsp.c
@@ -0,0 +1,69 @@
+#include <stdio.h>
+#include <gst/gst.h>
+
+
+int
+main (int argc, char *argv[])
+{
+
+  gboolean silent = FALSE;
+  GOptionContext *ctx;
+  GError *err = NULL;
+  GstElement *pipeline;
+  GstBus *bus;
+  GstMessage *msg;
+
+
+  // ***************************************************************************
+  // CUSTOM OPTION PARSING USING GLIB
+  //
+  // We can add additional configuration options here. 
+  // This is all glib stuff BTW
+  GOptionEntry entries[] = {
+    { "silent", 's', 0, G_OPTION_ARG_NONE, &silent,
+      "do not output status information", NULL },
+    { NULL }
+  };
+
+  // We create a context and add the entries
+  ctx = g_option_context_new ("RTSP Pipeline");
+  g_option_context_add_main_entries (ctx, entries, NULL);
+  g_option_context_add_group (ctx, gst_init_get_option_group ());
+  // We parse the options
+  if (!g_option_context_parse (ctx, &argc, &argv, &err)) {
+      g_print ("Failed to initialize: %s\n", err->message);
+      g_clear_error (&err);
+      g_option_context_free (ctx);
+      return 1;
+    }
+  g_option_context_free (ctx);
+  printf ("Run me with --help to see the Application options appended.\n");  
+  printf("silent mode %d", silent);
+
+  // ***************************************************************************
+  /* Initialize GStreamer */
+  gst_init (&argc, &argv);
+
+  /* Build the pipeline */
+  pipeline =
+      gst_parse_launch
+      ("playbin uri=https://www.freedesktop.org/software/gstreamer-sdk/data/media/sintel_trailer-480p.webm",
+      NULL);
+
+  /* Start playing */
+  gst_element_set_state (pipeline, GST_STATE_PLAYING);
+
+  /* Wait until error or EOS */
+  bus = gst_element_get_bus (pipeline);
+  msg =
+      gst_bus_timed_pop_filtered (bus, GST_CLOCK_TIME_NONE,
+      GST_MESSAGE_ERROR | GST_MESSAGE_EOS);
+
+  /* Free resources */
+  if (msg != NULL)
+    gst_message_unref (msg);
+  gst_object_unref (bus);
+  gst_element_set_state (pipeline, GST_STATE_NULL);
+  gst_object_unref (pipeline);
+  return 0;
+}
