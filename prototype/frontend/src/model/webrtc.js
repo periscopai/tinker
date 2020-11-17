3db0e27d2cceccf272ac56db5d501963fc5b91a3
Laurent Brack <laurent.brack@gmail.com>
2020-11-16 21:25:45 -0800

    Added some documentation. Very minor.

--- prototype/frontend/src/model/webrtc.js
+++ prototype/frontend/src/model/webrtc.js
@@ -28,6 +28,16 @@ class WebRTCToStreamingPipeline {
         }
     }
 
+    /** This is a promise which is called from the navigator.mediaDevices.getUserMedia
+     * 
+     * The stream object is assigned to the source object of the HTML5 video tag.
+     * 
+     * Note: That nothing else works after that thanks to the shitty webrtc stuff.
+     * 
+     * @param {*} stream A reference to a 
+     *                   [MediaStream](https://developer.mozilla.org/en-US/docs/Web/API/MediaStream) 
+     *                   object 
+     */
     onUserMediaSuccess(stream){
         console.log(stream);
         const video_element = document.querySelector('#periscopai_backend');
@@ -44,6 +54,9 @@ class WebRTCToStreamingPipeline {
         }
     }
 
+    /** Connects to the sequencer WebRTC Session.
+     * 
+     */
     connect(){
         this.log(`connecting to ${this.url} ${this.port}`);
 
@@ -65,10 +78,17 @@ class WebRTCToStreamingPipeline {
             }
         }
     }
+
+    /** Disconnect from the webRTC session.
+     * 
+     * Right now only pauses the video 
+     */
     disconnect(){
         this.log(`disconnecting from ${this.url} ${this.port}`);
         const video_element = document.querySelector('#periscopai_backend');
         if (video_element){
+            // This is the only trick I could find on stackoverflow to make the 
+            // video go away.
             this.log("Pausing the video")
             video_element.pause();
             video_element.currentTime = 0;
